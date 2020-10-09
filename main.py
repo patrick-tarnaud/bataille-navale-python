e70c69ef9d41a4c853e27701fade71aa71009f11
Patrick Tarnaud <patrick.tarnaud@gmail.com>
2020-10-09 15:01:58 +0200

    Ajout de fabriquer() et jouer()

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randint
 from model.Navire import *
 from model.Coque import *
 from model.Arme import *
@@ -16,18 +17,49 @@ def fabriquer():
     le_destructeur = Navire('Destroyer Le Destructeur', 'XRay', 2010, 40, 10000, 200000,
                             Coque(75, 'acier blindé', 'bleu'),
                             Arme('Canon', 75, 5000))
+    # print(la_fayette)
+    # print(le_narval)
+    # print(le_destructeur)
+    return la_fayette, le_narval, le_destructeur
+
+
+def jouer(la_fayette: Navire, le_narval : Navire, le_destructeur: Navire):
+    # tour 1
+    la_fayette.naviguer(1000)
+    la_fayette.tirer_sur(le_narval)
+
+    le_narval.naviguer(1000)
+    le_narval.tirer_sur(le_destructeur)
+
+    le_destructeur.naviguer(1000)
+    le_destructeur.tirer_sur(la_fayette)
+
+    # tour 2
+    la_fayette.naviguer(1000)
+    la_fayette.tirer_sur(le_destructeur)
+
+    le_narval.naviguer(1000)
+    le_narval.tirer_sur(la_fayette)
+
+    le_destructeur.naviguer(1000)
+    le_destructeur.tirer_sur(le_destructeur)
+
+    # tour 3
+    la_fayette.naviguer(1000)
+    la_fayette.tirer_sur(le_narval)
+
+    le_narval.naviguer(1000)
+    le_narval.tirer_sur(la_fayette)
+
+    le_destructeur.naviguer(1000)
+    le_destructeur.tirer_sur(la_fayette)
+
+    # resultat
     print(la_fayette)
     print(le_narval)
     print(le_destructeur)
-    return [la_fayette, le_narval, le_destructeur]
-
-
-def jouer(navires):
-    la_fayette, le_narval, le_destructeur = navires
-    la_fayette.naviguer(100)
-    la_fayette.tirer_sur(le_narval)
 
 
 if __name__ == '__main__':
     navires = fabriquer()
-    jouer(navires)
+    jouer(*navires)
--- model/Navire.py
+++ model/Navire.py
@@ -35,46 +35,55 @@ Etat : {etat}
            points_vie=self.coque.points_vie, etat=self.etat)
 
     def naviguer(self, distance):
-        self.kilometrage += distance
-        print("""{nom} navigue.
+        if self.etat == 'En marche':
+            self.kilometrage += distance
+            print("""{nom} navigue.
 {distance} NM parcourus.
 Kilométrage actuel : {kilometrage} NM.
--------------------------""".format(nom=self.nom, distance=distance, kilometrage=self.kilometrage))
+    -------------------------""".format(nom=self.nom, distance=distance, kilometrage=self.kilometrage))
+        else:
+            print(self.nom, 'ne peut plus naviguer car il est détruit')
 
     def subir_degats(self, degats):
-        # calcul des points de vie restant vis à vis des degats
-        if degats < self.coque.points_vie:
-            self.coque.points_vie -= degats
-            # la cible perd 5 points de resistance à chaque tir subi, ne peut être négatif
-            self.coque.resistance = self.coque.resistance - 5 if self.coque.resistance > 5 else 0
-        else:
-            # détruit
-            self.coque.points_vie = 0
+        if self.etat == 'En marche':
+            # calcul des points de vie restant vis à vis des degats
+            if degats < self.coque.points_vie:
+                self.coque.points_vie -= degats
+                # la cible perd 5 points de resistance à chaque tir subi, ne peut être négatif
+                self.coque.resistance = self.coque.resistance - 5 if self.coque.resistance > 5 else 0
+            else:
+                # détruit
+                self.coque.points_vie = 0
 
-        if self.etat == 'Détruit':
-            print(self.nom, 'est détruit !')
+            if self.etat == 'Détruit':
+                print(self.nom, 'est détruit !')
+        else:
+            print(self.nom, 'ne peut plus subir de dégâts car il est déjà détruit')
 
     def tirer_sur(self, cible):
-        print(self.nom, 'tire sur', cible.nom)
+        if self.arme.puissance_tir > 0:
+            print(self.nom, 'tire sur', cible.nom)
 
-        # calcul des degats
-        random1 = random.random()
-        random2 = random.random()
-        random3 = random.random()
-        degats = round((((0.5 + random1) * self.arme.puissance_tir) - (random2 * cible.coque.resistance)) * random3)
+            # calcul des degats
+            random1 = random.random()
+            random2 = random.random()
+            random3 = random.random()
+            degats = round((((0.5 + random1) * self.arme.puissance_tir) - (random2 * cible.coque.resistance)) * random3)
 
-        # La cible est 'en marche' et tir réussi
-        if degats > 0 and cible.etat == 'En marche':
-            print(degats, 'dégâts infligés')
-            cible.subir_degats(degats)
-        # la cible est deja détruite ou le tir échoué
-        else:
-            print('Le tir a échoué !')
+            # La cible est 'en marche' et tir réussi
+            if degats > 0 and cible.etat == 'En marche':
+                print(degats, 'dégâts infligés')
+                cible.subir_degats(degats)
+            # la cible est deja détruite ou le tir échoué
+            else:
+                print('Le tir a échoué !')
 
-        self.arme.puissance_tir = self.arme.puissance_tir - 1 if self.arme.puissance_tir > 0 else 0
+            self.arme.puissance_tir = self.arme.puissance_tir - 1 if self.arme.puissance_tir > 0 else 0
 
-        print("""Puissance {tireur} = {puissance_tir}.
+            print("""Puissance {tireur} = {puissance_tir}.
 Résistance {cible} = {resistance}.
 Points de vie {cible} = {points_vie}/100.
--------------------------""".format(tireur=self.nom, puissance_tir=self.arme.puissance_tir, cible=cible.nom,
-                                    resistance=cible.coque.resistance, points_vie=cible.coque.points_vie))
+    -------------------------""".format(tireur=self.nom, puissance_tir=self.arme.puissance_tir, cible=cible.nom,
+                                        resistance=cible.coque.resistance, points_vie=cible.coque.points_vie))
+        else:
+            print(self.nom, 'ne peut plus tirer car il n\'a plus de puissance de tir')
